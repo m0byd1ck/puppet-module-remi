68d1300d032fef41b14b91a807f70e316693bf44
m0byd1ck <m0byd1ck@users.noreply.github.com>
2015-08-20 20:27:54 +0100

    Fixed variables

--- manifests/rpm_gpg_key.pp
+++ manifests/rpm_gpg_key.pp
@@ -1,10 +1,11 @@
+# Define remi::rpm_gpg_key
 define remi::rpm_gpg_key($path) {
   # Given the path to a key, see if it is imported, if not, import it
-  exec {  "import-$name":
+  exec {  "import-${name}":
     path      => '/bin:/usr/bin:/sbin:/usr/sbin',
-    command   => "rpm --import $path",
+    command   => "rpm --import ${path}",
     unless    => "rpm -q gpg-pubkey-$(echo $(gpg --throw-keyids < $path) | cut --characters=11-18 | tr [A-Z] [a-z])",
-    require   => File[$path],
+    require   => File[${path}],
     logoutput => 'on_failure',
   }
 }
