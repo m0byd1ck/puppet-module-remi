aee6ae3de6612c98ab0ba42aeaba640d939dce2e
m0byd1ck <m0byd1ck@users.noreply.github.com>
2015-08-21 17:19:38 +0100

    Fixed missing space

--- manifests/params.pp
+++ manifests/params.pp
@@ -9,49 +9,49 @@ class remi::params {
     'gpgcheck'   => '1',
     'mirrorlist' => "http://rpms.famillecollet.com/enterprise/${os_maj_release}/remi/mirror",
     'include'    => undef,
-    'exclude'    =>undef
+    'exclude'    => undef
   }
   $php55          = {
     'enabled'  => '0',
     'gpgcheck' => '1',
     'baseurl'  => "http://rpms.famillecollet.com/enterprise/${os_maj_release}/php55/${os_basearch}/",
     'include'  => undef,
-    'exclude'  =>undef
+    'exclude'  => undef
   }
   $php56          = {
     'enabled'  => '1',
     'gpgcheck' => '1',
     'baseurl'  => "http://rpms.famillecollet.com/enterprise/${os_maj_release}/php56/${os_basearch}/",
     'include'  => undef,
-    'exclude'  =>undef
+    'exclude'  => undef
   }
   $php70          = {
     'enabled'  => '0',
     'gpgcheck' => '1',
     'baseurl'  => "http://rpms.famillecollet.com/enterprise/${os_maj_release}/php70/${os_basearch}/",
     'include'  => undef,
-    'exclude'  =>undef
+    'exclude'  => undef
   }
   $test           = {
     'enabled'    => '0',
     'gpgcheck'   => '1',
     'mirrorlist' => "http://rpms.famillecollet.com/enterprise/${os_maj_release}/test/mirror",
     'include'    => undef,
-    'exclude'    =>undef
+    'exclude'    => undef
   }
   $debuginfo      = {
     'enabled'  => '0',
     'gpgcheck' => '1',
     'baseurl'  => "http://rpms.famillecollet.com/enterprise/${os_maj_release}/debug-remi/${os_basearch}/",
     'include'  => undef,
-    'exclude'  =>undef
+    'exclude'  => undef
   }
   $test_debuginfo = {
     'enabled'  => '0',
     'gpgcheck' => '1',
     'baseurl'  => "http://rpms.famillecollet.com/enterprise/${os_maj_release}/debug-test/${os_basearch}/",
     'include'  => undef,
-    'exclude'  =>undef
+    'exclude'  => undef
   }
 
 }
